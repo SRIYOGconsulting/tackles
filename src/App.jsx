7cf1290afee6178fe4487f63080ad7a354a6a4a2
Sneha-Neupanee <neupanesneha648@gmail.com>
2025-10-31 07:59:15 +0545

    refactor: move to vite from create react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Pricing from "./pages/Pricing";
-import Projects from "./pages/Projects";
-import Blog from "./pages/Blog";
-import Areas from "./pages/Areas";
-import Contact from "./pages/Contact";
-import Legal from "./pages/Legal";
-import Dubai from "./pages/Dubai";
-import Sydney from "./pages/Sydney";
-import SanFrancisco from "./pages/SanFrancisco";
-
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/areas" element={<Areas />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/legal" element={<Legal />} />
-        <Route path="/dubai" element={<Dubai />} />
-        <Route path="/sydney" element={<Sydney />} />
-        <Route path="/sanfrancisco" element={<SanFrancisco />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <h1 className="bg-red-500">Hello world!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi aliquid quaerat dicta consectetur dolore suscipit tempora explicabo eum reiciendis aspernatur?</p>
+    </>
+  )
 }
